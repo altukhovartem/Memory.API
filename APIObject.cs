ef9a395db0caede3a233a6a8a4bceb0d4d1e4fee
Altukhov Artem <altukhov.artem.dev@gmail.com>
2018-10-08 18:37:07 +0300

    done

--- APIObject.cs
+++ APIObject.cs
@@ -1,10 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Memory.API
 {
-    
+    public class APIObject: IDisposable
+    {
+        private object _state;
+        
+        public APIObject(int x)
+        {
+            _state = x;
+            MagicAPI.Allocate(x);
+        }
+
+        ~APIObject()
+        {
+            MyOwnDispose();
+        }
+
+        public void Dispose()
+        {
+            MyOwnDispose();
+        }
+
+        private void MyOwnDispose()
+        {
+            if (_state != null)
+            {
+                MagicAPI.Free((int)_state);
+                _state = null;
+            }
+        }
+    }
 }
